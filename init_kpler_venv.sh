c60d73bf1945e88c463dbf1b0a03e66eec16f6d7
David Steffes <david.h.steffes@gmail.com>
2025-04-21 13:40:25 -0500

    fixing little bug w/ subprocess

--- init_kpler_venv.sh
+++ init_kpler_venv.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# 1) make the venv if it doesn't exist
-if [ ! -d "kpler-env" ]; then
+# only create once
+if [ ! -d kpler-env ]; then
   python3 -m venv kpler-env
 fi
 
-# 2) install exactly the Kpler SDK (and its pandas<=1.5.3) into that venv
-kpler-env/bin/pip install --upgrade pip
-kpler-env/bin/pip install kpler.sdk==1.0.53
+# install exactly what Kpler needs
+./kpler-env/bin/pip install --upgrade pip
+./kpler-env/bin/pip install -r kpler_requirements.txt
+
