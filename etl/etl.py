89dec95c004649d5ed21d2418cb9db10a2a55437
David Steffes <david.h.steffes@gmail.com>
2025-04-25 17:17:15 -0500

    transforming cron job, by sequestering all api gathering into etl to deal with mismatches in python libraries

--- etl/etl.py
+++
@@ -1,8 +0,0 @@
-# etl/etl.py
-
-from app.modules.data_processing import update_all
-
-# Only refresh API datasets once a day
-update_all()   # api_only=True by default
-
-print("✅ API pipelines updated.")
